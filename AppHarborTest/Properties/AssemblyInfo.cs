342e6f11b14fb0612dc56bafe95270f4c6e0116c
apresh chandra <apreshchandra3@gmail.com>
2013-02-27 12:48:04 +0530

    third-webforms
    third-webforms

--- AppHarborTest/Properties/AssemblyInfo.cs
+++ AppHarborTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PungryMeister")]
+[assembly: AssemblyTitle("AppHarborTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Smnetserv")]
-[assembly: AssemblyProduct("PungryMeister")]
+[assembly: AssemblyProduct("AppHarborTest")]
 [assembly: AssemblyCopyright("Copyright © Smnetserv 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bdd7e4ea-2d75-466a-aed0-2968f0b10b84")]
+[assembly: Guid("8ac3b120-1b24-4e1d-8370-3c2a10ff8ddb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
